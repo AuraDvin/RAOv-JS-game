331c01cf0a8d55e39541bcea57894ebff6ad96cb
AuraDvin <edvin.garibovic@gmail.com>
2022-12-09 23:58:05 +0100

    Made changes to movement Handler [WIP]

--- scripts/game.js
+++ scripts/game.js
@@ -1,19 +1,19 @@
 import { Player } from './player.js';
+import { downPressed, rightPressed, leftPressed, upPressed, handleMovement, handleStopping } from './movementHandler.js';
+
 
 let player;
 let canvas;
 let ctx;
 let cameraOffset = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
 let cameraZoom = 1;
+let isRunning = true;
 const MAX_ZOOM = 2;
 const MIN_ZOOM = 0.4;
 const SCROLL_SENSITIVITY = 0.0005
 let lastRender = 0;
-let upPressed = false;
-let downPressed = false;
-let leftPressed = false;
-let rightPressed = false;
 let assets = "../assets/";
+let directionalMovement = ['w', 'a', 's', 'd'];
 
 
 addEventListener('DOMContentLoaded', () => {
@@ -24,18 +24,21 @@ addEventListener('DOMContentLoaded', () => {
   canvas.height = 1080;
 
   ctx = canvas.getContext('2d');
-  ctx.translate(window.innerWidth / 2, window.innerHeight / 2);
-  ctx.scale(cameraZoom, cameraZoom);
-  ctx.translate(-window.innerWidth / 2 + cameraOffset.x, -window.innerHeight / 2 + cameraOffset.y);
+  // ctx.translate(window.innerWidth / 2, window.innerHeight / 2);
+  // ctx.scale(cameraZoom, cameraZoom);
+  // ctx.translate(-window.innerWidth / 2 + cameraOffset.x, -window.innerHeight / 2 + cameraOffset.y);
   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  // console.log({x:window.innerHeight,y:window.innerHeight});
 
-
-  // console.log("loaded");
-  document.addEventListener('keydown', keyDownHandler, false);
-  document.addEventListener('keyup', keyUpHandler, false);
-  // document.addEventListener('wheel', scaleHandler, false);
+  addEventListener('keydown', keyDownHandler, false);
+  addEventListener('keyup', keyUpHandler, false);
+  addEventListener('wheel', scaleHandler, false);
+  document.body.onmouseover = ()=>{
+    canvas.focus();
+  }
   window.requestAnimationFrame(loop);
 });
+
 
 //! beta feature (limit zoom not added yet :P)
 function scaleHandler(event) {
@@ -43,36 +46,27 @@ function scaleHandler(event) {
   cameraZoom += event.deltaY * SCROLL_SENSITIVITY;
   cameraZoom = Math.min(cameraZoom, MAX_ZOOM);
   cameraZoom = Math.max(cameraZoom, MIN_ZOOM);
-  ctx.scale(cameraZoom,cameraZoom);
-  ctx.translate(-window.innerWidth / 2 + cameraOffset.x, -window.innerHeight / 2 + cameraOffset.y);
-  console.log(cameraZoom);
+  ctx.scale(cameraZoom, cameraZoom);
+  // ctx.translate(-window.innerWidth / 2 + cameraOffset.x, -window.innerHeight / 2 + cameraOffset.y);
+  // console.log(cameraZoom);
 }
 
 async function keyDownHandler(event) {
-  // console.log(`${event.keyCode} pressed`);
-  if (event.key === 'd' || event.key === 'ArrowRight') {
-    rightPressed = true;
-  } else if (event.key === 'a' || event.key === 'ArrowLeft') {
-    leftPressed = true;
+  if (!directionalMovement.includes(event.key)) {
+    switch (event.key) {
+      case 'Escape': isRunning = !isRunning; break;
+    }
+  } else {
+    if (isRunning) handleMovement(event);
   }
-  if (event.key === 'w' || event.key === 'ArrowUp') {
-    upPressed = true;
-  } else if (event.key === 's' || event.key === 'ArrowDown') {
-    downPressed = true;
-  }
+
 }
 
 async function keyUpHandler(event) {
-  if (event.key === 'd' || event.key === 'ArrowRight') {
-    rightPressed = false;
-  } else if (event.key === 'a' || event.key === 'ArrowLeft') {
-    leftPressed = false;
+  if (directionalMovement.includes(event.key)) {
+    handleStopping(event);
   }
-  if (event.key === 'w' || event.key === 'ArrowUp') {
-    upPressed = false;
-  } else if (event.key === 's' || event.key === 'ArrowDown') {
-    downPressed = false;
-  }
+  
 }
 
 //! Game loop
@@ -84,41 +78,47 @@ async function loop(timestamp) {
   lastRender = timestamp;
   window.requestAnimationFrame(loop);
 }
-  
 
-  async function draw() {
-    let playerx = player.getPosition().x;
-    let playery = player.getPosition().y;
-    // ctx.clearRect(0,0, window.innerWidth, window.innerHeight);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-    ctx.drawImage(player.getSprite(), player.getPosition().x, player.getPosition().y);
-  
+
+async function draw() {
+  let playerx = player.getPosition().x;
+  let playery = player.getPosition().y;
+  // ctx.clearRect(0,0, window.innerWidth, window.innerHeight);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.drawImage(player.getSprite(), player.getPosition().x, player.getPosition().y);
+
+}
+
+
+async function update(progress) {
+  // console.log(progress);
+  let move = {
+    x: 0,
+    y: 0
   }
-  
-  
-  async function update(progress) {
-    let move = {
-      x: 0,
-      y: 0
-    }
-    //. Up is negative, down is positive (origin in topleft)
-    if (upPressed) {
-      move.y = -player.speed * 1 / progress;
-    } else if (downPressed) {
-      move.y = player.speed * 1 / progress;
-    }
-  
-    if (rightPressed) {
-      move.x = player.speed * 1 / progress;
-    } else if (leftPressed) {
-      move.x = -player.speed * 1 / progress;
-    }
-  
-  
-    if (move.x == move.y || -move.x == move.y) {
-      move.x *= 0.5;
-      move.y *= 0.5;
-    }
-    player.move(move);
+  //. Up is negative, down is positive (origin in topleft)
+  if (upPressed) {
+    move.y = -player.speed;
+  } else if (downPressed) {
+    move.y = player.speed;
   }
+
+  if (rightPressed) {
+    move.x = player.speed;
+  } else if (leftPressed) {
+    move.x = -player.speed;
+  }
+
+
+  if ((move.x == move.y || -move.x == move.y) && (move.x != 0 || move.y == 0)) {
+    move.x = player.diagSpeed * Math.sign(move.x);
+    move.y = player.diagSpeed * Math.sign(move.y);
+  }
+
+
+  move.x *= progress;
+  move.y *= progress;
+  // console.log(move);
+  player.move(move);
+}
--- scripts/player.js
+++ scripts/player.js
@@ -1,46 +1,58 @@
 export class Player {
   #sprite
   #health
-  speed
   #name
   #position
-  constructor(img,name) {
+  speed
+  diagSpeed
+
+  constructor(img, name) {
     this.#name = name;
     this.#health = 100;
-    this.speed = 200;
-    
-    this.#sprite = new Image(256,256); 
+    this.#sprite = new Image(256, 256);
     this.#sprite.src = img;
-    this.#position = {x:0,y:0};
+    this.#position = { x: 0, y: 0 };
+    
+    this.speed = 2;
+    // This just makes it so we move diagonally at this.speed
+    this.diagSpeed = Math.floor(this.speed * Math.sqrt(1 / 2));
   }
 
-
-  die() {
-    console.log("You died");
+  // The bounds are going to change depending on the level size (not decided yet)
+  #checkInBounds() {
+    let limitX = 1920;
+    let limitY = 1080;
+    this.#position.x = Math.min(Math.max(this.#position.x, 0), limitX - this.#sprite.width);
+    this.#position.y = Math.min(Math.max(this.#position.y, 0), limitY - this.#sprite.height);
   }
+  // comment the not needed code for now
+  // die() {
+  //   console.log("You died");
+  // }
 
-  takeDamage (source) {
-    this.#health -= source.damage;
-    if (this.#health < 0) 
-      this.die();  
-  }
+  // takeDamage(source) {
+  //   this.#health -= source.damage;
+  //   if (this.#health < 0)
+  //     this.die();
+  // }
 
-  getSprite(){
+  getSprite() {
     return this.#sprite;
   }
 
- getName(){
-  return this.#name;
- }
+  // getName() {
+  //   return this.#name;
+  // }
 
- getPosition() {
-  return {x:this.#position.x, y: this.#position.y};
- }
+  getPosition() {
+    return { x: this.#position.x, y: this.#position.y };
+  }
 
- move (vector2) {
+  // Funcion to with moving character to new position
+  move(vector2) {
     this.#position.x += vector2.x;
     this.#position.y += vector2.y;
-    // console.log(this.#position);
- }
+    this.#checkInBounds();
+  }
 
 }
