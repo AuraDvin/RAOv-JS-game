fdb93494e7acb1ba6aea9d2ed55377d2b343c9f7
AuraDvin <edvin.garibovic@gmail.com>
2022-12-10 17:20:21 +0100

    Fixed problems with player movement

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,10 @@
 import { Player } from './player.js';
-import { downPressed, rightPressed, leftPressed, upPressed, handleMovement, handleStopping } from './movementHandler.js';
-
+import { KeysHandler, KeysPressed } from './movementHandler.js';
 
 let player;
+/** @type {HTMLCanvasElement} */
 let canvas;
+/** @type {CanvasRenderingContext2D} */
 let ctx;
 let cameraOffset = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
 let cameraZoom = 1;
@@ -13,13 +14,11 @@ const MIN_ZOOM = 0.4;
 const SCROLL_SENSITIVITY = 0.0005
 let lastRender = 0;
 let assets = "../assets/";
-let directionalMovement = ['w', 'a', 's', 'd'];
 
 
 addEventListener('DOMContentLoaded', () => {
   player = new Player(assets + 'player.png', 'Bingus Janez');
-
-  canvas = document.getElementById("gameView");
+  canvas = document.getElementById("player");
   canvas.width = 1920;
   canvas.height = 1080;
 
@@ -27,15 +26,12 @@ addEventListener('DOMContentLoaded', () => {
   // ctx.translate(window.innerWidth / 2, window.innerHeight / 2);
   // ctx.scale(cameraZoom, cameraZoom);
   // ctx.translate(-window.innerWidth / 2 + cameraOffset.x, -window.innerHeight / 2 + cameraOffset.y);
+  // drawBG();
   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  // console.log({x:window.innerHeight,y:window.innerHeight});
-
-  addEventListener('keydown', keyDownHandler, false);
-  addEventListener('keyup', keyUpHandler, false);
   addEventListener('wheel', scaleHandler, false);
-  document.body.onmouseover = ()=>{
-    canvas.focus();
-  }
+  addEventListener('keydown', KeysHandler, false);
+  addEventListener('keyup', KeysHandler, false);
+  addEventListener('mouseover', () => { canvas.focus(); });
   window.requestAnimationFrame(loop);
 });
 
@@ -51,74 +47,53 @@ function scaleHandler(event) {
   // console.log(cameraZoom);
 }
 
-async function keyDownHandler(event) {
-  if (!directionalMovement.includes(event.key)) {
-    switch (event.key) {
-      case 'Escape': isRunning = !isRunning; break;
-    }
-  } else {
-    if (isRunning) handleMovement(event);
-  }
-
-}
-
-async function keyUpHandler(event) {
-  if (directionalMovement.includes(event.key)) {
-    handleStopping(event);
-  }
-  
-}
 
 //! Game loop
 async function loop(timestamp) {
   let progress = timestamp - lastRender;
-  // console.log(progress);
+  lastRender = timestamp;
+
   update(progress);
   draw();
-  lastRender = timestamp;
+
   window.requestAnimationFrame(loop);
 }
 
 
 async function draw() {
-  let playerx = player.getPosition().x;
-  let playery = player.getPosition().y;
-  // ctx.clearRect(0,0, window.innerWidth, window.innerHeight);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
   ctx.drawImage(player.getSprite(), player.getPosition().x, player.getPosition().y);
 
 }
 
 
 async function update(progress) {
-  // console.log(progress);
   let move = {
     x: 0,
     y: 0
   }
   //. Up is negative, down is positive (origin in topleft)
-  if (upPressed) {
+  if (KeysPressed['w']) {
     move.y = -player.speed;
-  } else if (downPressed) {
+  } else if (KeysPressed['s']) {
     move.y = player.speed;
   }
 
-  if (rightPressed) {
+  if (KeysPressed['d']) {
     move.x = player.speed;
-  } else if (leftPressed) {
+  } else if (KeysPressed['a']) {
     move.x = -player.speed;
   }
 
-
-  if ((move.x == move.y || -move.x == move.y) && (move.x != 0 || move.y == 0)) {
+  if ((move.x) && (move.x == move.y || -move.x == move.y)) {
     move.x = player.diagSpeed * Math.sign(move.x);
     move.y = player.diagSpeed * Math.sign(move.y);
   }
-
-
   move.x *= progress;
   move.y *= progress;
-  // console.log(move);
+
   player.move(move);
+
+
+
 }
